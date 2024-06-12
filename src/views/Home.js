239a8b5e159da9a42a4b5da97af971873e1ba05f
Laurensia Dini <adhimxtkj@gmail.com>
2024-06-12 14:06:23 +0800

    update: view home

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouse, faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { faHouse, faArrowLeft, faArrowRight, faCirclePlay } from '@fortawesome/free-solid-svg-icons';
 import './Home.css';
 
 const Home = () => {
@@ -24,8 +24,7 @@ const Home = () => {
                     <p className='font-2 text-white text-justify mt-5'>perfect for architects, designers, &
                     home ownwers, our VR walkthroughs bring <span className='fw-bold'>your visions to life</span>
                      with stunning <span className='fw-bold'>realism</span></p>
-
-                    <button className='btn dp-block mt-5'>See playlist</button>
+                    <button className='btn dp-block mt-5'>See playlist<FontAwesomeIcon icon={faArrowRight} size="1x" className='mx-2'/></button>
                 </div>
                 <div className='w-50'>
                     <div className='row'>
@@ -106,29 +105,70 @@ const Home = () => {
                     </p>
                 </div>
             </div>
-            <div className='d-flex section-card mx-5 mt-4'>
-                <div className='card mx-2'>
-                    <img src="cardphoto.png" className="logo" alt="logo" />
-                    <div className='card-body'>
-                        <p className='p-2 card-titlle text-start fw-bold'>iVr's Showcase Series . Bakery/Pastry shop . SC05</p>
-                        <p className='p-2 card-description text-justify'>Immerse yourself in this luxurios enviroment with our VR walkthroughs
-                        . See how chic black tones and elegant design create a stylish, serene space</p>
+            <div class='scroll-container mx-5 mt-4 mb-5'>
+                <div className='section-card p-3'>
+                    <div className='card-slide-active mx-2'>
+                        <div className='dp-flex p-3'>
+                            <img src="cardphoto.png" className="logo-card w-25" alt="logo" />
+                            <div className='w-75 mt-3'>
+                                <p className='text-start fs-28'>Amelia Joseph</p>
+                                <p className='fw-bold text-start fs-15'>Chief Manager</p>
+                            </div>
+                        </div>
+                        <div className='card-body padding-4'>
+                            <p className='text-justify'>Use this section to describe your company and the products you offer. You could share your company’s story and details about why you are in business.</p>
+                        </div>
+                    </div>
+                    <div className='card-slide mx-2'>
+                        <div className='dp-flex p-3'>
+                            <img src="cardphoto.png" className="logo-card w-25" alt="logo" />
+                            <div className='w-75 mt-3'>
+                                <p className='text-start fs-28'>Jacob Joshua</p>
+                                <p className='fw-bold text-start fs-15'>Chief Manager</p>
+                            </div>
+                        </div>
+                        <div className='card-body padding-4'>
+                            <p className='text-justify'>Use this section to describe your company and the products you offer. You could share your company’s story and details about why you are in business.</p>
+                        </div>
+                    </div>
+                    <div className='card-slide mx-2'>
+                        <div className='dp-flex p-3'>
+                            <img src="cardphoto.png" className="logo-card w-25" alt="logo" />
+                            <div className='w-75 mt-3'>
+                                <p className='text-start fs-28'>Jacob Joshua</p>
+                                <p className='fw-bold text-start fs-15'>Chief Manager</p>
+                            </div>
+                        </div>
+                        <div className='card-body padding-4'>
+                            <p className='text-justify'>Use this section to describe your company and the products you offer. You could share your company’s story and details about why you are in business.</p>
+                        </div>
+                    </div>
+                    <div className='card-slide mx-2'>
+                        <div className='dp-flex p-3'>
+                            <img src="cardphoto.png" className="logo-card w-25" alt="logo" />
+                            <div className='w-75 mt-3'>
+                                <p className='text-start fs-28'>Jacob Joshua</p>
+                                <p className='fw-bold text-start fs-15'>Chief Manager</p>
+                            </div>
+                        </div>
+                        <div className='card-body padding-4'>
+                            <p className='text-justify'>Use this section to describe your company and the products you offer. You could share your company’s story and details about why you are in business.</p>
+                        </div>
                     </div>
                 </div>
-                <div className='card mx-2'>
-                    <img src="cardphoto.png" className="logo" alt="logo" />
-                    <div className='card-body'>
-                        <p className='p-2 card-titlle text-start fw-bold'>iVr's Showcase Series . Bakery/Pastry shop . SC05</p>
-                        <p className='p-2 card-description text-justify'>Immerse yourself in this luxurios enviroment with our VR walkthroughs
-                        . See how chic black tones and elegant design create a stylish, serene space</p>
-                    </div>
-                </div>
-                <div className='card mx-2'>
-                    <img src="cardphoto.png" className="logo" alt="logo" />
-                    <div className='card-body'>
-                        <p className='p-2 card-titlle text-start fw-bold'>iVr's Showcase Series . Bakery/Pastry shop . SC05</p>
-                        <p className='p-2 card-description text-justify'>Immerse yourself in this luxurios enviroment with our VR walkthroughs
-                        . See how chic black tones and elegant design create a stylish, serene space</p>
+            </div>
+            <div className='mt-5 p-50 mb-5'>
+                <div className='container-sub padding-10'>
+                    <p className='text-sub text-end' data-text="Subscribe To Our">Subscribe To Our</p>
+                    <div className='dp-flex'>
+                        <div className='w-65'>
+                            <img src='imgsub.png' className='logo-sub'/>
+                        </div>
+                        <div className='w-35'>
+                            <p className='text-sub text-end'>Updates</p>
+                            <p className='text-sub-des text-end'>Stay in the loop with latest developments as we welcome the future, together.</p>
+                            <p className='text-white text-end fs-5'><FontAwesomeIcon className='mx-2' icon={faCirclePlay} size="1x"/>View Past Newsletters</p>
+                        </div>
                     </div>
                 </div>
             </div>
